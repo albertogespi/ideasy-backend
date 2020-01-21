9c87633071dc32953e1cf540be6b225851674993
Alberto <alberto.gespi@gmail.com>
2020-01-21 20:43:29 +0100

    AGE - descargado paquete CORS

--- app/webserver/index.js
+++ app/webserver/index.js
@@ -1,9 +1,9 @@
 "use strict";
 
 const express = require("express");
-
 const app = express();
-
+const cors = require("cors");
+app.use(cors()); //esto permite la conexión frontend-backend, en teoría
 app.use(express.json());
 
 const { accountRouter, userRouter } = require("./routes");
