54a70e8e18599e2499e76ed1c58496e3d89c03d7
Alberto <alberto.gespi@gmail.com>
2020-02-04 18:44:24 +0100

    AGE - close-project

--- app/webserver/routes/project-router.js
+++ app/webserver/routes/project-router.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 
+const closeProject = require("../controllers/project/close-project-controller");
 const getProject = require("../controllers/project/get-project-controller");
 const followProject = require("../controllers/project/follow-project-controller");
 const checkAccount = require("../controllers/account/check-account-controller");
@@ -10,5 +11,6 @@ const router = express.Router();
 
 router.get("/project/:projectId", checkAccount, getProject);
 router.post("/project/:projectId", checkAccount, followProject);
+router.delete("project/:projectId", checkAccount, closeProject);
 
 module.exports = router;
