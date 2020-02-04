cc9be1996ee59d816f3a0175cc07a76c325da828
Alberto <alberto.gespi@gmail.com>
2020-02-04 20:09:35 +0100

    AGE - backend acabado en principio aleluya oh señor

--- app/webserver/routes/projects-router.js
+++ app/webserver/routes/projects-router.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 
-// const createProject = require("../controllers/projects/create-project-controller");
+const createProject = require("../controllers/projects/create-project-controller");
 const getOrgProjects = require("../controllers/projects/get-org-projects-controller");
 const getFollowedProjects = require("../controllers/projects/get-dev-followed-projects-controller");
 const getContributedProjects = require("../controllers/projects/get-dev-contributed-projects-controller");
@@ -14,6 +14,6 @@ router.get("/projects/contributed/:userId", getContributedProjects);
 router.get("/projects/followed/:userId", getFollowedProjects);
 router.get("/projects/ratings/:userId", getAvgRatings);
 router.get("/projects/:userId", getOrgProjects);
-// router.post("/projects/new", createProject);
+router.post("/projects/new", createProject);
 
 module.exports = router;
