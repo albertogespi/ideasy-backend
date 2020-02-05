1642ce7c0968fbc8c70d491706acdf66824d76bd
Alberto <alberto.gespi@gmail.com>
2020-02-05 12:37:45 +0100

    AGE

--- app/webserver/controllers/project/get-project-controller.js
+++ app/webserver/controllers/project/get-project-controller.js
@@ -3,24 +3,26 @@
 const mysqlPool = require("../../../database/mysql-pool");
 
 async function getProject(req, res, next) {
-	const { projectId } = req.params;
+  const { projectId } = req.params;
 
-	try {
-		const connection = await mysqlPool.getConnection();
+  try {
+    const connection = await mysqlPool.getConnection();
 
-		const sqlQuery = `SELECT * FROM projectsAndFollowers WHERE project_id = "${projectId}"`;
-		const [rows] = await connection.execute(sqlQuery);
-		connection.release();
+    const sqlQuery = `SELECT * FROM projectsAndFollowers WHERE project_id = "${projectId}"`;
+    const [rows] = await connection.execute(sqlQuery);
+    connection.release();
 
-		if (rows.length !== 1) {
-			return res.status(404).send("el proyecto no existe");
-		}
+    if (rows.length !== 1) {
+      return res.status(404).send("el proyecto no existe");
+    }
 
-		return res.status(200).send(rows);
-	} catch (e) {
-		console.error(e);
-		return res.status(500).send();
-	}
+    const [project] = rows;
+
+    return res.status(200).send(project);
+  } catch (e) {
+    console.error(e);
+    return res.status(500).send();
+  }
 }
 
 module.exports = getProject;
