a01e3a18e65d9b431f71bca83687dfc77a87ac2a
Aitana <aitanammtz@gmail.com>
2020-02-05 14:15:22 +0100

    Conflictos

--- app/webserver/controllers/projects/get-dev-avg-doc-rating-controller.js
+++ app/webserver/controllers/projects/get-dev-avg-doc-rating-controller.js
@@ -3,21 +3,23 @@
 const mysqlPool = require("../../../database/mysql-pool");
 
 async function getRating(req, res, next) {
-  const { userId } = req.params;
+	const { userId } = req.params;
 
-  try {
-    const connection = await mysqlPool.getConnection();
-    const sqlQuery = `SELECT COALESCE(AVG(rating), 0) AS rating_average 
+	try {
+		const connection = await mysqlPool.getConnection();
+		const sqlQuery = `SELECT COALESCE(AVG(rating), 0) AS rating_average 
     FROM documents WHERE user_id = ?`;
 
-    const [rating] = await connection.execute(sqlQuery, [userId]);
-    connection.realease();
+		const [rows] = await connection.execute(sqlQuery, [userId]);
+		connection.release();
 
-    return res.status(200).send(rating);
-  } catch (e) {
-    console.error(e);
-    return res.status(500).send();
-  }
+		const [rating] = rows;
+
+		return res.status(200).send(rating);
+	} catch (e) {
+		console.error(e);
+		return res.status(500).send();
+	}
 }
 
 module.exports = getRating;
