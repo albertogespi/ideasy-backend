6f288432cdae44cbd1555ce69abd6c46c406de51
Aitana <aitanammtz@gmail.com>
2020-02-15 12:52:37 +0100

    AMM - base de datos cambiada

--- app/webserver/controllers/documents/get-documents-controller.js
+++ app/webserver/controllers/documents/get-documents-controller.js
@@ -3,23 +3,23 @@
 const mysqlPool = require("../../../database/mysql-pool");
 
 async function getDocuments(req, res, next) {
-  const { projectId } = req.params;
+	const { projectId } = req.params;
 
-  try {
-    const connection = await mysqlPool.getConnection();
-    const sqlQuery = `SELECT * FROM documents WHERE project_id = ?`;
-    const [documents] = await connection.execute(sqlQuery, [projectId]);
-    connection.release();
+	try {
+		const connection = await mysqlPool.getConnection();
+		const sqlQuery = `SELECT * FROM documentsAndUsers WHERE project_id = ?`;
+		const [documents] = await connection.execute(sqlQuery, [projectId]);
+		connection.release();
 
-    if (documents.length === 0) {
-      return res.status(404).send("no se ha encontrado el proyecto");
-    }
+		if (documents.length === 0) {
+			return res.status(404).send("no se ha encontrado el proyecto");
+		}
 
-    return res.status(200).send(documents);
-  } catch (e) {
-    console.error(e);
-    return res.status(500).send();
-  }
+		return res.status(200).send(documents);
+	} catch (e) {
+		console.error(e);
+		return res.status(500).send();
+	}
 }
 
 module.exports = getDocuments;
--- app/webserver/controllers/documents/upload-document-controller.js
+++ app/webserver/controllers/documents/upload-document-controller.js
@@ -5,78 +5,74 @@ const mysqlPool = require("../../../database/mysql-pool");
 const uuid = require("uuid/v4");
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
+	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 async function uploadDocument(req, res, next) {
-  const { file } = req;
-  const docId = uuid();
-  const { projectId } = req.params;
-  const { title } = req.body;
-  const { userId, role } = req.claims;
+	const { file } = req;
+	const docId = uuid();
+	const { projectId } = req.params;
+	const { title } = req.body;
+	const { userId, role } = req.claims;
 
-  if (!file || !file.buffer) {
-    return res.status(400).send({
-      message: "invalid document"
-    });
-  }
+	if (!file || !file.buffer) {
+		return res.status(400).send({
+			message: "invalid document",
+		});
+	}
 
-  // if (role !== "dev") {
-  //   return res.status(403).send();
-  // }
+	cloudinary.uploader
+		.upload_stream(
+			{
+				resource_type: "raw",
+				public_id: userId,
+				format: "pdf",
+			},
+			async (err, result) => {
+				if (err) {
+					console.error(err);
+					return res.status(400).send(err);
+				}
 
-  cloudinary.uploader
-    .upload_stream(
-      {
-        resource_type: "raw",
-        public_id: userId,
-        format: "pdf"
-      },
-      async (err, result) => {
-        if (err) {
-          console.error(err);
-          return res.status(400).send(err);
-        }
+				const secureUrl = result.secure_url;
 
-        const secureUrl = result.secure_url;
+				let connection;
+				try {
+					connection = await mysqlPool.getConnection();
+					const now = new Date()
+						.toISOString()
+						.replace("T", " ")
+						.substring(0, 19);
 
-        let connection;
-        try {
-          connection = await mysqlPool.getConnection();
-          const now = new Date()
-            .toISOString()
-            .replace("T", " ")
-            .substring(0, 19);
-
-          const sqlQuery = `INSERT INTO documents 
+					const sqlQuery = `INSERT INTO documents 
           SET doc_id = ?, project_id = ?, 
           user_id = ?, title = ?, 
           file_url = ?, uploaded_at = ?`;
 
-          connection.query(sqlQuery, [
-            docId,
-            projectId,
-            userId,
-            title,
-            secureUrl,
-            now
-          ]);
-          connection.release();
+					connection.query(sqlQuery, [
+						docId,
+						projectId,
+						userId,
+						title,
+						secureUrl,
+						now,
+					]);
+					connection.release();
 
-          res.header("Location", secureUrl);
-          return res.status(201).send("documento subido");
-        } catch (e) {
-          if (connection) {
-            connection.release();
-          }
-          console.error(e);
-          return res.status(500).send(e.message);
-        }
-      }
-    )
-    .end(file.buffer);
+					res.header("Location", secureUrl);
+					return res.status(201).send("documento subido");
+				} catch (e) {
+					if (connection) {
+						connection.release();
+					}
+					console.error(e);
+					return res.status(500).send(e.message);
+				}
+			},
+		)
+		.end(file.buffer);
 }
 
 module.exports = uploadDocument;
