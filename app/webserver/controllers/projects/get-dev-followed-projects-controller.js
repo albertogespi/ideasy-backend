690bd59ccdfc06ac33d8ab0ef855cf2efdb7c3b7
Aitana <aitanammtz@gmail.com>
2020-02-17 21:16:24 +0100

    AMM - cambios en projects

--- app/webserver/controllers/projects/get-dev-followed-projects-controller.js
+++ app/webserver/controllers/projects/get-dev-followed-projects-controller.js
@@ -4,34 +4,34 @@
 const mysqlPool = require("../../../database/mysql-pool");
 
 async function getFollowedProjects(req, res, next) {
-  const { userId } = req.params;
-  const filters = req.query;
+	const { userId } = req.params;
+	const filters = req.query;
 
-  let connection;
-  try {
-    connection = await mysqlPool.getConnection();
+	let connection;
+	try {
+		connection = await mysqlPool.getConnection();
 
-    //query to obtain the data
-    let sqlQuery = `SELECT projects.* FROM projects INNER JOIN users_projects ON projects.project_id = users_projects.project_id WHERE users_projects.user_id = "${userId}"`;
+		//query to obtain the data
+		let sqlQuery = `SELECT projectsAndFollowers.* FROM projectsAndFollowers INNER JOIN users_projects ON projectsAndFollowers.project_id = users_projects.project_id WHERE users_projects.user_id = "${userId}"`;
 
-    if (Object.keys(filters).length !== 0) {
-      for (const filter in filters) {
-        sqlQuery += ` AND ${filter} = "${filters[filter]}"`;
-      }
-    }
+		if (Object.keys(filters).length !== 0) {
+			for (const filter in filters) {
+				sqlQuery += ` AND ${filter} = "${filters[filter]}"`;
+			}
+		}
 
-    const [data] = await connection.execute(sqlQuery);
-    connection.release();
+		const [data] = await connection.execute(sqlQuery);
+		connection.release();
 
-    return res.status(200).send(data);
-  } catch (e) {
-    if (connection) {
-      connection.release();
-    }
+		return res.status(200).send(data);
+	} catch (e) {
+		if (connection) {
+			connection.release();
+		}
 
-    console.error(e);
-    return res.status(500).send();
-  }
+		console.error(e);
+		return res.status(500).send();
+	}
 }
 
 module.exports = getFollowedProjects;
