1369ff8a497349143faad42e184efa03d7d10ad5
Aitana <aitanammtz@gmail.com>
2020-02-20 19:15:50 +0100

    AMM - get-home-projects-controller.js ya filtra los que están cerrados

--- app/webserver/controllers/home/get-home-projects-controller.js
+++ app/webserver/controllers/home/get-home-projects-controller.js
@@ -4,41 +4,33 @@
 const mysqlPool = require("../../../database/mysql-pool");
 
 async function getHomeProjects(req, res, next) {
-  const filters = req.query;
+	const filters = req.query;
 
-  let connection;
-  try {
-    connection = await mysqlPool.getConnection();
+	let connection;
+	try {
+		connection = await mysqlPool.getConnection();
 
-    //query to obtain the data
-    let sqlQuery = "SELECT * FROM projectsAndFollowers";
+		//query to obtain the data
+		let sqlQuery = "SELECT * FROM projectsAndFollowers WHERE closed_at IS NULL";
 
-    if (Object.keys(filters).length !== 0) {
-      sqlQuery += " WHERE";
+		if (Object.keys(filters).length !== 0) {
+			for (const filter in filters) {
+				sqlQuery += ` AND ${filter} = "${filters[filter]}"`;
+			}
+		}
 
-      let firstPos = true;
-      for (const filter in filters) {
-        if (firstPos) {
-          sqlQuery += ` ${filter} = "${filters[filter]}"`;
-          firstPos = false;
-        } else {
-          sqlQuery += ` AND ${filter} = "${filters[filter]}"`;
-        }
-      }
-    }
+		const [data] = await connection.execute(sqlQuery);
+		connection.release();
 
-    const [data] = await connection.execute(sqlQuery);
-    connection.release();
+		return res.status(200).send(data);
+	} catch (e) {
+		if (connection) {
+			connection.release();
+		}
 
-    return res.status(200).send(data);
-  } catch (e) {
-    if (connection) {
-      connection.release();
-    }
-
-    console.error(e);
-    return res.status(500).send();
-  }
+		console.error(e);
+		return res.status(500).send();
+	}
 }
 
 module.exports = getHomeProjects;
