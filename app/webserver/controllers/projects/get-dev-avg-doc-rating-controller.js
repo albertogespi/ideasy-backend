16ff79d9e1c4d6e98ec23e7979c2821d6c81a303
Aitana <aitanammtz@gmail.com>
2020-02-21 21:47:55 +0100

    AMM - delete is null en avg doc rating

--- app/webserver/controllers/projects/get-dev-avg-doc-rating-controller.js
+++ app/webserver/controllers/projects/get-dev-avg-doc-rating-controller.js
@@ -3,23 +3,23 @@
 const mysqlPool = require("../../../database/mysql-pool");
 
 async function getAvgRatings(req, res, next) {
-  const { userId } = req.params;
+	const { userId } = req.params;
 
-  try {
-    const connection = await mysqlPool.getConnection();
-    const sqlQuery = `SELECT COALESCE(AVG(rating), 0) AS rating_average 
-    FROM documents WHERE user_id = ?`;
+	try {
+		const connection = await mysqlPool.getConnection();
+		const sqlQuery = `SELECT COALESCE(AVG(rating), 0) AS rating_average 
+    FROM documents WHERE user_id = ? WHERE deleted_at IS NULL`;
 
-    const [rows] = await connection.execute(sqlQuery, [userId]);
-    connection.release();
+		const [rows] = await connection.execute(sqlQuery, [userId]);
+		connection.release();
 
-    const [rating] = rows;
+		const [rating] = rows;
 
-    return res.status(200).send(rating.rating_average);
-  } catch (e) {
-    console.error(e);
-    return res.status(500).send();
-  }
+		return res.status(200).send(rating.rating_average);
+	} catch (e) {
+		console.error(e);
+		return res.status(500).send();
+	}
 }
 
 module.exports = getAvgRatings;
